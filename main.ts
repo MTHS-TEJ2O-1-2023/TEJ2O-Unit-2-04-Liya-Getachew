e9a1aee1895e55d42ae59b4ca29106f4eb124b45
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-09-21 13:16:59 -0400

    ts version

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Liya Getachew
+ * Created on: Sep 2023
+ * This program shows the temperature.
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+//  variable
+let temperature: number
+
+input.onButtonPressed(Button.A, function () {
+  temperature = input.temperature()
+  basic.showString('The temperature is ' + temperature + 'C.')
+}) 
